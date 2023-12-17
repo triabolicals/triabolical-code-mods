fe750d966bfa1f20e58581d195684f181cf6d59d
Tri Le <trilehuynh@gmail.com>
2023-12-17 13:40:43 -0500

    Updated for Exp mode, Gift Mode, and Negative Growths

--- src/cutscene.rs
+++ src/cutscene.rs
@@ -3,6 +3,8 @@ use unity::prelude::*;
 use engage::menu::{BasicMenuResult, config::{ConfigBasicMenuItemSwitchMethods, ConfigBasicMenuItem}};
 use engage::gamevariable::*;
 pub const CUTSCENES_KEY: &str = "G_CUTSCENE";
+pub const BGM_KEY: &str = "G_BGM";
+pub const GIFT_KEY: &str = "G_GIFT";
 
 pub struct CutsceneMod;
 pub fn patchCutscenes(){
@@ -18,15 +20,10 @@ pub fn patchCutscenes(){
         Patch::in_text(0x01ed8e20).bytes(&[0xFD, 0x7B, 0xBE, 0xA9]).unwrap();
         Patch::in_text(0x01ed8ef0).bytes(&[0xFD, 0x7B, 0xBD, 0xA9]).unwrap();
         println!("Cutscenes/Movies are not skipped");
-
     }
 }
-
-
 impl ConfigBasicMenuItemSwitchMethods for CutsceneMod {
-    fn init_content(this: &mut ConfigBasicMenuItem){ 
-        patchCutscenes();
-    }
+    fn init_content(this: &mut ConfigBasicMenuItem){  patchCutscenes(); }
     extern "C" fn custom_call(this: &mut ConfigBasicMenuItem, _method_info: OptionalMethod) -> BasicMenuResult {
         let toggle = GameVariableManager::get_bool(CUTSCENES_KEY);
         let result = ConfigBasicMenuItem::change_key_value_b(toggle);
@@ -50,9 +47,107 @@ impl ConfigBasicMenuItemSwitchMethods for CutsceneMod {
         else { this.command_text = format!("Off").into(); }
     }
 }
+pub struct BGMmod;
+impl ConfigBasicMenuItemSwitchMethods for BGMmod {
+    fn init_content(this: &mut ConfigBasicMenuItem){ 
+        GameVariableManager::make_entry_norewind(BGM_KEY, 0);
+
+     }
+    extern "C" fn custom_call(this: &mut ConfigBasicMenuItem, _method_info: OptionalMethod) -> BasicMenuResult {
+        let toggle = GameVariableManager::get_bool(BGM_KEY);
+        let result = ConfigBasicMenuItem::change_key_value_b(toggle);
+        if toggle != result {
+            GameVariableManager::set_bool(BGM_KEY, result );
+            Self::set_command_text(this, None);
+            Self::set_help_text(this, None);
+            this.update_text();
+            return BasicMenuResult::se_cursor();
+        } else {return BasicMenuResult::new(); }
+    }
+    extern "C" fn set_help_text(this: &mut ConfigBasicMenuItem, _method_info: OptionalMethod){
+        let toggle = GameVariableManager::get_bool(BGM_KEY);
+        if (toggle) { this.help_text = format!("Player phase BGM overrides other phase BGM.").into(); } 
+        else { this.help_text = format!("Default BGM Setting").into(); }
+    }
+    extern "C" fn set_command_text(this: &mut ConfigBasicMenuItem, _method_info: OptionalMethod){
+        let toggle = GameVariableManager::get_bool(BGM_KEY);
+        if (toggle) { this.command_text = format!("Skip Enemy/Ally Phase").into();} 
+        else { this.command_text = format!("Default").into(); }
+    }
+}
+
+//Force BGM to player phase
+#[skyline::hook(offset=0x02d56700)]
+pub fn ChangeBGM(this: u64, forceType: i32, proc: u64, isTurn: bool, method_info: OptionalMethod) {
+    let toggle = GameVariableManager::get_bool(BGM_KEY);
+    if toggle { call_original!(this, 0, proc, isTurn, method_info) }
+    else { call_original!(this, forceType, proc, isTurn,method_info) }
+}
+//For Time Crystals Phase Change
+#[skyline::hook(offset=0x02d56930)]
+pub fn ChangeBGM2(this: u64, forceType: i32, method_info: OptionalMethod) {
+    println!("Change force IMMM called with force {}", forceType);
+    let toggle = GameVariableManager::get_bool(BGM_KEY);
+    if toggle { call_original!(this, 0,  method_info) }
+    else { call_original!(this, forceType, method_info) }
+}
+
+pub struct Giftmod;
+impl ConfigBasicMenuItemSwitchMethods for Giftmod {
+    fn init_content(this: &mut ConfigBasicMenuItem){  GameVariableManager::make_entry_norewind(GIFT_KEY, 0); }
+    extern "C" fn custom_call(this: &mut ConfigBasicMenuItem, _method_info: OptionalMethod) -> BasicMenuResult {
+        let toggle = GameVariableManager::get_number(GIFT_KEY);
+        let result = ConfigBasicMenuItem::change_key_value_i(toggle, 0, 3, 1);
+        if toggle != result {
+            GameVariableManager::set_number(GIFT_KEY, result );
+            Self::set_command_text(this, None);
+            Self::set_help_text(this, None);
+            this.update_text();
+            return BasicMenuResult::se_cursor();
+        } else {return BasicMenuResult::new(); }
+    }
+    extern "C" fn set_help_text(this: &mut ConfigBasicMenuItem, _method_info: OptionalMethod){
+        let toggle = GameVariableManager::get_number(GIFT_KEY);
+        if toggle == 0 { this.help_text = format!("Accept both patch and DLC gift items.").into(); } 
+        else if toggle == 1 { this.help_text = format!("Accept only patch items.").into(); } 
+        else if toggle == 2 { this.help_text = format!("Accept only paid DLC items.").into(); }
+        else if toggle == 3 { this.help_text = format!("Accept no gifts.").into(); }
+    }
+    extern "C" fn set_command_text(this: &mut ConfigBasicMenuItem, _method_info: OptionalMethod){
+        let toggle = GameVariableManager::get_number(GIFT_KEY);
+        if toggle == 0 { this.command_text = format!("All Gifts").into();} 
+        else if toggle == 1 { this.command_text = format!("Patch Update Gifts").into(); }
+        else if toggle == 2 { this.command_text = format!("Expansion Pass Gifts").into(); }
+        else if toggle == 3 { this.command_text = format!("No Gifts").into(); }
+    }
+}
+
+#[skyline::hook(offset= 0x023f3c00)]
+pub fn TryGiftEvent(this: u64, flagName: &Il2CppString, rewardID: &Il2CppString, messageID: &Il2CppString, method_info: OptionalMethod){
+    let toggle = GameVariableManager::get_number(GIFT_KEY);
+    if toggle == 0{ call_original!(this, flagName, rewardID, messageID, method_info); }
+    else if toggle == 1 {
+        if messageID.get_string().unwrap() == "MID_MSG_GET_ITEM_DLC_Accessory1" {return; }
+        if messageID.get_string().unwrap() == "MID_MSG_GET_ITEM_DLC_Accessory2" {return; }
+        call_original!(this, flagName, rewardID, messageID, method_info);
+    }
+    else if toggle == 2 {
+        if messageID.get_string().unwrap() == "MID_MSG_GET_ITEM_Patch0" {return; }
+        if messageID.get_string().unwrap() == "MID_MSG_GET_ITEM_Patch3" {return; }
+        call_original!(this, flagName, rewardID, messageID, method_info);
+    }
+    else { return;  }
+
+
+}
+
+
 #[no_mangle]
 extern "C" fn cutscene() -> &'static mut ConfigBasicMenuItem { ConfigBasicMenuItem::new_switch::<CutsceneMod>("Skip Cutscenes/Movies") }
-
+extern "C" fn bgm() -> &'static mut ConfigBasicMenuItem { ConfigBasicMenuItem::new_switch::<BGMmod>("Battle BGM Settings") }
+extern "C" fn gift() -> &'static mut ConfigBasicMenuItem { ConfigBasicMenuItem::new_switch::<Giftmod>("Gift Options") }
 pub fn cutscene_install(){
    cobapi::install_game_setting(cutscene);
+   cobapi::install_game_setting(bgm);
+   cobapi::install_game_setting(gift);
 }
--- src/level.rs
+++ src/level.rs
@@ -7,6 +7,7 @@ use engage::gameuserdata::GameUserData;
 use engage::{gamedata::*, singleton::SingletonClass};
 use engage::{gamedata::person::*, gamedata::unit::*};
 
+//Character, Level, Growth Mods
 pub const CHARACTER_KEY: &str = "G_CHARACTER";
 pub const LEVEL_DIS_KEY: &str = "G_LEVEL_TYPE";
 pub const GROWTH_KEY: &str = "G_GROWTH_TYPE";
@@ -14,6 +15,8 @@ pub static mut setPerson: bool = false;
 pub const NameArray : &[&str] = &["MPID_Lueur", "MPID_Vandre", "MPID_Clan", "MPID_Fram", "MPID_Alfred", "MPID_Etie", "MPID_Boucheron", "MPID_Celine", "MPID_Chloe", "MPID_Louis", "MPID_Yunaka", "MPID_Staluke", "MPID_Citrinica", "MPID_Lapis", "MPID_Diamand", "MPID_Umber", "MPID_Jade", "MPID_Ivy", "MPID_Kagetsu", "MPID_Zelkova", "MPID_Fogato", "MPID_Pandoro", "MPID_Bonet", "MPID_Misutira", "MPID_Panetone", "MPID_Merin", "MPID_Hortensia", "MPID_Seadas", "MPID_Rosado", "MPID_Goldmary", "MPID_Linden", "MPID_Saphir", "MPID_Veyre", "MPID_Mauve", "MPID_Anna", "MPID_Jean" ];
 pub const OptionArray : &[&str] = &["None", "Vander", "Clanne", "Framme", "Alfred", "Etie", "Boucheron", "Céline", "Chloé", "Louis", "Yunaka", "Alcryst", "Citrinne", "Lapis", "Diamant", "Amber", "Jade", "Ivy", "Kagetsu", "Zelkov", "Fogato", "Pandero", "Bunet", "Timerra", "Panette", "Merrin", "Hortensia", "Seadall", "Rosado", "Goldmary", "Linden", "Saphir", "Veyle", "Mauvier", "Anna", "Jean"];
 pub const HelpArray : &[&str] = &["MPID_H_Lueur", "MPID_H_Vandre", "MPID_H_Clan", "MPID_H_Fram", "MPID_H_Alfred", "MPID_H_Etie", "MPID_H_Boucheron", "MPID_H_Celine", "MPID_H_Chloe", "MPID_H_Louis", "MPID_H_Yunaka", "MPID_H_Staluke", "MPID_H_Citrinica", "MPID_H_Lapis", "MPID_H_Diamand", "MPID_H_Umber", "MPID_H_Jade", "MPID_H_Ivy", "MPID_H_Kagetsu", "MPID_H_Zelkova", "MPID_H_Fogato", "MPID_H_Pandoro", "MPID_H_Bonet", "MPID_H_Misutira", "MPID_H_Panetone", "MPID_H_Merin", "MPID_H_Hortensia", "MPID_H_Seadas", "MPID_H_Rosado", "MPID_H_Goldmary", "MPID_H_Linden", "MPID_H_Saphir", "MPID_H_Veyre", "MPID_H_Mauve", "MPID_H_Anna", "MPID_H_Jean" ];
+pub const PIDArray : &[&str] = &["PID_リュール", "PID_ヴァンドレ", "PID_クラン", "PID_フラン", "PID_アルフレッド", "PID_エーティエ", "PID_ブシュロン", "PID_セリーヌ", "PID_クロエ", "PID_ルイ", "PID_ユナカ", "PID_スタルーク", "PID_シトリニカ", "PID_ラピス", "PID_ディアマンド", "PID_アンバー", "PID_ジェーデ", "PID_アイビー", "PID_カゲツ", "PID_ゼルコバ", "PID_フォガート", "PID_パンドロ", "PID_ボネ", "PID_ミスティラ", "PID_パネトネ", "PID_メリン", "PID_オルテンシア", "PID_セアダス", "PID_ロサード", "PID_ゴルドマリー", "PID_リンデン", "PID_ザフィーア", "PID_ヴェイル", "PID_モーヴ", "PID_アンナ", "PID_ジャン"];
+pub static mut playerIndex: usize = 0;
 
 #[unity::class("TMPro", "TextMeshProUGUI")]
 pub struct TextMeshProUGUI {}
@@ -22,7 +25,6 @@ pub struct TextMeshProUGUI {}
 pub struct UnitStatusSetter {
     junk: [u8; 376],
     level: &'static UnitStatusSetter_ValueParam,
-    //
 }
 
 #[unity::class("App", "UnitStatusSetter_ValueParam")]
@@ -45,15 +47,12 @@ pub fn UnitInfo_SetLevel(this: &UnitInfoParamSetter, unit: Option<&Unit>, x: i32
     match unit {
         Some(p) => {
             unsafe {
-
-            GameVariableManager::make_entry_norewind(LEVEL_DIS_KEY, 0);
-            let result = GameVariableManager::get_bool(LEVEL_DIS_KEY);
-            let enLevel = unit_GetEnhancedLevel(p, None);
-            let mut displayed_level = enLevel;
-            if result {
-                displayed_level = enLevel + (p.m_InternalLevel as i32);
-            }
-            TrySetText(this.m_level, displayed_level, None);
+                GameVariableManager::make_entry_norewind(LEVEL_DIS_KEY, 0);
+                let result = GameVariableManager::get_bool(LEVEL_DIS_KEY);
+                let enLevel = unit_GetEnhancedLevel(p, None);
+                let mut displayed_level = enLevel;
+                if result { displayed_level = enLevel + (p.m_InternalLevel as i32); }
+                TrySetText(this.m_level, displayed_level, None);
             }
         },
         None => {},
@@ -80,9 +79,7 @@ pub fn Set__Level(this: &UnitStatusSetter, unit: &Unit, unit_no_enhance: &Unit, 
         let max_level = jobdata_get_max_level(unit_no_enhance.m_Job, None);
         let boost: i32 = (no_enLevel < enLevel) as i32;
         let at_limit: bool = max_level <= unit_level;
-
         let displayed_level = enLevel;
-
         if result {
             let internal_level = unit_no_enhance.m_InternalLevel;
             if internal_level == 0{
@@ -93,7 +90,6 @@ pub fn Set__Level(this: &UnitStatusSetter, unit: &Unit, unit_no_enhance: &Unit, 
                 let level_str = format!("{}/{}", displayed_level, internal_level).into();
                 SetValueDirect(this.level, level_str , boost, at_limit, None);
             }
-
         }
         else {
             let level_str = format!("{}", displayed_level).into();
@@ -101,6 +97,14 @@ pub fn Set__Level(this: &UnitStatusSetter, unit: &Unit, unit_no_enhance: &Unit, 
         }
     }
 }
+#[skyline::from_offset(0x02487990)]
+pub fn skillarray_find(this: &SkillArray, sid: &Il2CppString, method_info: OptionalMethod) -> Option<u64>;
+
+#[skyline::from_offset(0x02482850)]
+pub fn skillarray_remove(this: &SkillArray, sid: &Il2CppString, method_info: OptionalMethod) -> bool;
+
+#[skyline::from_offset(0x02482850)]
+pub fn skillarray_add(this: &SkillArray, sid: &Il2CppString, method_info: OptionalMethod) -> bool;
 
 fn restoreDefault(){
     //Growth Mode Call
@@ -110,7 +114,10 @@ fn restoreDefault(){
     //Random RNG 
     Patch::in_text(0x01a3a73c).bytes(&[0x5d, 0xeb, 0x24, 0x94]).unwrap();
     //Fixed
-    Patch::in_text(0x01a3a410).bytes(&[0x14,0x81,0x40, 0x39]).unwrap();
+    Patch::in_text(0x01a3a410).bytes(&[0x14,0x81, 0x40, 0x39]).unwrap();
+    // Level Down but add the level instead of subtracting it
+    Patch::in_text(0x01a3ac8c).bytes(&[0x08, 0x05, 0x0, 0x51]).unwrap();
+
 }
 pub fn patchGrowth(){
     GameVariableManager::make_entry_norewind(GROWTH_KEY, 0);
@@ -144,28 +151,35 @@ pub fn patchGrowth(){
         Patch::in_text(0x01a3a73c).bytes(&[0x20, 0x00, 0x80, 0x52]).unwrap();
         println!("Growth set to 'Perfect'");
     }
+    else if result == 4 {
+        //negative growths 
+        restoreDefault(); 
+        Patch::in_text(0x01a3ac8c).bytes(&[0x08, 0x05, 0x00, 0x11]).unwrap();
+        println!("Growth set to 'Negative'");
+    }
 }
 pub fn set_Person(){
     let triabolical = PersonData::get_list_mut().expect("triabolical is 'None'");
     let t_list = &triabolical.list.items;
     let seted = unsafe { setPerson };
-
     if seted == false {
         unsafe {
-            println!("Copying playable characters to unused character slots");
-            for x in 2..33 {   
-                set_person(t_list[x], t_list[902+x]);
+            for x in 0..PIDArray.len() {
+                let person = PersonData::get(PIDArray[x]);
+                match person {
+                    Some(p) => { 
+                        set_person(p, t_list[902+x]); 
+                        match skillarray_find(get_CommonSkill(p, None), "SID_主人公".into(), None) {
+                            Some(i) => { 
+                                playerIndex = x; 
+                                println!("Protag Skill found on person #{} - {}", x, NameArray[x]);
+                            }
+                            None => {}
+                        }
+                    },
+                    None => {}
+                }
             }
-            // Veyle 39
-            set_person(t_list[39], t_list[935]);
-            // Mauvier 49
-            set_person(t_list[49], t_list[936]);
-            // Anna 51 
-            set_person(t_list[51], t_list[937]);
-            // Jean 52
-            set_person(t_list[52], t_list[938]);
-            // Alear 1
-            set_person(t_list[1], t_list[939]);
         }
     }
     unsafe { setPerson = true; }
@@ -178,73 +192,49 @@ pub fn changeCharacters(){
     set_Person();
     if result == 0 {
         unsafe {
-            for x in 2..33 {   
-                set_person(t_list[902+x], t_list[x]);
-                set_name(t_list[x], NameArray[x-1].into(), None);
-                set_help(t_list[x], HelpArray[x-1].into(), None);
+            //Default!
+            for x in 0..PIDArray.len() {
+                let person = PersonData::get(PIDArray[x]);
+                match person {
+                    Some(p) => {
+                        set_person(t_list[902+x], p); 
+                        set_name(p, NameArray[x].into(), None);
+                        set_help(p, HelpArray[x].into(), None);
+                    },
+                    None => {}
+                }
             }
-            // Veyle 39
-            set_person(t_list[935], t_list[39]);
-            set_name(t_list[39], NameArray[32].into(), None);
-            set_help(t_list[39], HelpArray[32].into(), None);
-            // Mauvier 49
-            set_person(t_list[936], t_list[49]);
-            set_name(t_list[49], NameArray[33].into(), None);
-            set_help(t_list[49], HelpArray[33].into(), None);
-
-            // Anna 51 
-            set_person(t_list[937], t_list[51]);
-            set_name(t_list[51], NameArray[34].into(), None);
-            set_help(t_list[51], HelpArray[34].into(), None);
-
-            // Jean 52
-            set_person(t_list[938], t_list[52]);
-            set_name(t_list[52], NameArray[35].into(), None);
-            set_help(t_list[52], HelpArray[35].into(), None);
-
-            set_person(t_list[939], t_list[1]);
-            set_name(t_list[1], NameArray[0].into(), None);
-            set_help(t_list[1], HelpArray[0].into(), None);
-            println!("Characters are set to default.");
         }
+        println!("Characters are set to default.");
     }
     else {
-        let index: usize = (903 + result).try_into().unwrap();
+        let index: usize = (902 + result).try_into().unwrap();
         let index2: usize = (result).try_into().unwrap();
         let name = NameArray[index2];
         let help = HelpArray[index2];
         unsafe {
-            for x in 2..33 {   
-                set_person(t_list[index], t_list[x]);
-                set_name(t_list[x], name.into(), None);
-                set_help(t_list[x], help.into(), None);
+            for x in 0..PIDArray.len() {
+                let person = PersonData::get(PIDArray[x]);
+                match person {
+                    Some(p) => {
+                        set_person(t_list[index], p); 
+                        set_name(p, name.into(), None);
+                        set_help(p, help.into(), None);
+                        let sids = get_CommonSkill(p, None);
+                        //Remove Protag Skill
+                        if x != playerIndex {
+                            if skillarray_remove(sids, "SID_主人公".into(), None) { set_CommonSkill(p, sids, None); }
+                        }
+                    },
+                    None => {}
+                }
             }
-            // Veyle 39
-            set_person(t_list[index], t_list[39]);
-            set_name(t_list[39], name.into(), None);
-            set_help(t_list[39], help.into(), None);
-            // Mauvier 49
-            set_person(t_list[index], t_list[49]);
-            set_name(t_list[49], name.into(), None);
-            set_help(t_list[49], help.into(), None);
-
-            // Anna 51 
-            set_person(t_list[index], t_list[51]);
-            set_name(t_list[51], name.into(), None);
-            set_help(t_list[51], help.into(), None);
-
-            // Jean 52
-            set_person(t_list[index], t_list[52]);
-            set_name(t_list[52], name.into(), None);
-            set_help(t_list[52], help.into(), None);
-
-            set_person(t_list[index], t_list[1]);
-            set_name(t_list[1], name.into(), None);
-            set_help(t_list[1], help.into(), None);
-            
-            let skill_array_lueur = get_CommonSkill(t_list[939], None);
-            set_CommonSkill(t_list[1], skill_array_lueur, None);
-
+            let skill_array_lueur = get_CommonSkill(t_list[902+playerIndex], None);
+            let person = PersonData::get(PIDArray[playerIndex]);
+            match person {
+                Some(p) => {set_CommonSkill(p, skill_array_lueur, None);}
+                None => {}
+            }
             println!("Characters are set to {}", name);
         }
     }
@@ -256,23 +246,7 @@ impl ConfigBasicMenuItemSwitchMethods for CharacterMod {
         let triabolical = PersonData::get_list_mut().expect("triabolical is 'None'");
         let t_list = &triabolical.list.items;
         let seted = unsafe { setPerson };
-        if seted == false {
-            unsafe {
-                for x in 2..33 {   
-                    set_person(t_list[x], t_list[902+x]);
-                }
-                // Veyle 39
-                set_person(t_list[39], t_list[935]);
-                // Mauvier 49
-                set_person(t_list[49], t_list[936]);
-                // Anna 51 
-                set_person(t_list[51], t_list[937]);
-                // Jean 52
-                set_person(t_list[52], t_list[938]);
-                // Lueur
-                set_person(t_list[1], t_list[939]);
-            }
-        }
+        if seted == false { set_Person(); }
         unsafe { setPerson = true; }
         changeCharacters();
     }
@@ -291,12 +265,8 @@ impl ConfigBasicMenuItemSwitchMethods for CharacterMod {
     }
     extern "C" fn set_help_text(this: &mut ConfigBasicMenuItem, _method_info: OptionalMethod){
         let typeC =  GameVariableManager::get_number(CHARACTER_KEY);
-        if typeC == 0 {
-            this.help_text = format!("Characters are set their default appearance/growths.").into(); 
-        }
-        else {
-            this.help_text = format!("Characters are set to the selected character.").into(); 
-        }
+        if typeC == 0 { this.help_text = format!("Characters are set their default appearance/growths.").into();  }
+        else { this.help_text = format!("Characters are set to the selected character.").into();   }
     }
     extern "C" fn set_command_text(this: &mut ConfigBasicMenuItem, _method_info: OptionalMethod){
         let typeC: usize = (GameVariableManager::get_number(CHARACTER_KEY)).try_into().unwrap();
@@ -305,12 +275,10 @@ impl ConfigBasicMenuItemSwitchMethods for CharacterMod {
 }
 pub struct GrowthMod;
 impl ConfigBasicMenuItemSwitchMethods for  GrowthMod {
-    fn init_content(this: &mut ConfigBasicMenuItem){
-        patchGrowth();
-    }
+    fn init_content(this: &mut ConfigBasicMenuItem){ patchGrowth(); }
     extern "C" fn custom_call(this: &mut ConfigBasicMenuItem, _method_info: OptionalMethod) -> BasicMenuResult {
         let toggle =  GameVariableManager::get_number(GROWTH_KEY);
-        let result = ConfigBasicMenuItem::change_key_value_i(toggle, 0, 3, 1);
+        let result = ConfigBasicMenuItem::change_key_value_i(toggle, 0, 4, 1);
 
         if toggle != result {
             GameVariableManager::set_number(GROWTH_KEY, result);
@@ -334,7 +302,7 @@ impl ConfigBasicMenuItemSwitchMethods for  GrowthMod {
         }
         else if typeC == 2 { this.help_text = format!("Units will not gain stats on level ups.").into(); }
         else if typeC == 3 { this.help_text = format!("Units will gain perfect level ups.").into();  }
-        else {this.help_text = format!("Unknown Setting").into(); }
+        else if typeC == 4 {this.help_text = format!("Units will lose stats on level up.").into(); }
     }
     extern "C" fn set_command_text(this: &mut ConfigBasicMenuItem, _method_info: OptionalMethod){
         let type_C =  GameVariableManager::get_number(GROWTH_KEY);
@@ -349,7 +317,7 @@ impl ConfigBasicMenuItemSwitchMethods for  GrowthMod {
         }
         else if type_C == 2 { this.command_text = format!("No Growths").into(); }
         else if type_C == 3 { this.command_text = format!("Perfect Growths").into();  }
-        else {this.help_text = format!("Unknown").into(); }
+        else if type_C == 4 { this.command_text = format!("Negative Growths").into(); }
     }
 }
 pub struct LevelMod;
@@ -379,17 +347,11 @@ impl ConfigBasicMenuItemSwitchMethods for LevelMod {
 }
 
 #[no_mangle]
-extern "C" fn level_callback() -> &'static mut ConfigBasicMenuItem { 
-    ConfigBasicMenuItem::new_switch::<LevelMod>("Unit Level Display")
-}
+extern "C" fn level_callback() -> &'static mut ConfigBasicMenuItem {  ConfigBasicMenuItem::new_switch::<LevelMod>("Unit Level Display")}
 #[no_mangle]
-extern "C" fn growth_callback() -> &'static mut ConfigBasicMenuItem { 
-    ConfigBasicMenuItem::new_switch::<GrowthMod>("Growth Mode")
-}
+extern "C" fn growth_callback() -> &'static mut ConfigBasicMenuItem { ConfigBasicMenuItem::new_switch::<GrowthMod>("Growth Mode")}
 #[no_mangle]
-extern "C" fn char_callback() -> &'static mut ConfigBasicMenuItem { 
-    ConfigBasicMenuItem::new_switch::<CharacterMod>("Single Character Mode")
-}
+extern "C" fn char_callback() -> &'static mut ConfigBasicMenuItem { ConfigBasicMenuItem::new_switch::<CharacterMod>("Single Character Mode")}
 pub fn level_install(){
     cobapi::install_game_setting(growth_callback);
     cobapi::install_game_setting(level_callback);
